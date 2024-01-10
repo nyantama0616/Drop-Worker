4d8ca6241ec8facae968c83154f30f4a09bba44b
nyantama <r_nyantama0616@yahoo.co.jp>
2024-01-10 23:16:52 +0900

    Initial Commit

---
+++ Drop&Worker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drop&Worker
+//
+//  Created by Nyantama on 2024/01/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drop&Worker/Drop_WorkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drop_WorkerApp.swift
+//  Drop&Worker
+//
+//  Created by Nyantama on 2024/01/10.
+//
+
+import SwiftUI
+
+@main
+struct Drop_WorkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
