ce8c4c5cd6ab7f0c33f5c8624a6e7d210ec5dd2d
nyantama <r_nyantama0616@yahoo.co.jp>
2024-01-10 23:31:17 +0900

    カウンターアプリ作成
    ボタンを押すとcount upする

--- Drop&Worker/ContentView.swift
+++ Drop&Worker/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(String(count))
+            Button(action: {count += 1}) {
+                Text("Count Up")
+            }
         }
         .padding()
     }
